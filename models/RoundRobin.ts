531e63e0cb364c335ae0831bfcff16c4e42a557d
Allrivenjs <jaimeruizxdd2@gmail.com>
2022-10-29 17:04:28 -0500

    update

--- models/RoundRobin.ts
+++ models/RoundRobin.ts
@@ -1,4 +1,4 @@
-import { Process } from './procces';
+import {IProcess, Process} from './procces';
 import {IGroupProcess} from "./groupProcesses";
 
 
@@ -7,19 +7,29 @@ import {IGroupProcess} from "./groupProcesses";
 // 2. ejecutar los procesos por turnos, cada proceso tiene un quantum
 // 3. eliminar los archivos de los procesos
 // 4. terminar;
-export const roundRobin = (processCatalog: IGroupProcess, quantum: number) => {
-    let processCatalogCopy = processCatalog;
 
-    // createFilesByProcess(processCatalog);
 
-    while (true) {
-        for (let i = 0; i < quantum; i++) {
-         //   executeProcess(actualProcess);
-        }
-        if (1==1){
-            break;
-        }
-    }
+
+// tenemos que tener un IGroupProcess que tenga un array de procesos, y recibimos el quantum, entonces:
+// debemos ejecutar solo un proceso del grupo de procesos, y en ese proce debemos asignarle un nombre que sera el nombre del proceso,
+// y tambien una descripcion, que sera la descripcion del grupo de procesos y el contenido asocitivo al proceso
+
+
+// se debe definir que el procesos se pueda contener en un array de listos, en un array (siempre sera un proceso) de ejecucion y en un array de terminados
+// el algoritmo debe permitir pausar el hilo de ejecucion, y debe permitir reanudar el hilo ( entiendase como hilo el procesos que se esta ejecutando ) de ejecucion (esto es para el front)
+
+
+// para poder resolver eso,
+//
+
+
+let arrayReady: Array<Promise<IProcess>> = [];
+let arrayExecution: Array<Process> = [];
+let arrayFinished: Array<Process> = [];
+
+
+export const roundRobin = (groupProcess: IGroupProcess, quantum: number) => {
+
 };
 
 
