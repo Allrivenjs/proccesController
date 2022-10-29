5a4443a6f571616db5b71eb99e4aacf6c0dbd953
Allrivenjs <jaimeruizxdd2@gmail.com>
2022-10-29 16:38:44 -0500

    update

--- models/RoundRobin.ts
+++ models/RoundRobin.ts
@@ -1,19 +1,27 @@
 import { Process } from './procces';
+import {IGroupProcess} from "./groupProcesses";
 
-export const roundRobin = (processCatalog: Array<Process>, quantum: number) => {
+
+// el algoritmo de round robin es:
+// 1. crear los archivos de los procesos
+// 2. ejecutar los procesos por turnos, cada proceso tiene un quantum
+// 3. eliminar los archivos de los procesos
+// 4. terminar;
+export const roundRobin = (processCatalog: IGroupProcess, quantum: number) => {
     let processCatalogCopy = processCatalog;
-    
-    let actualProcess = processCatalog[0];
 
-    createFilesByProcess(processCatalog);
+    // createFilesByProcess(processCatalog);
 
-    while(true) {
+    while (true) {
         for (let i = 0; i < quantum; i++) {
-            executeProcess(actualProcess);
-        };
-        break;
-    };
+         //   executeProcess(actualProcess);
+        }
+        if (1==1){
+            break;
+        }
+    }
 };
+
 
 const executeProcess = (process: Process) => {
     // TODO:
@@ -26,12 +34,12 @@ const createFilesByProcess = (processCatalog: Array<Process>) => {
         // llamar al helper para crear archivos con:
         // - el nombre del proceso en cuestión
         // - la ruta, que sea por defecto ./process_files o algo así
-    };
+    }
 };
 
 const deleteFilesByProcess = (processCatalog: Array<Process>) => {
     for (const process of processCatalog) {
         // TODO:
         // llamar al helper para eliminar los archivos de los procesos y limpiar esa vaina:
-    };
+    }
 };
