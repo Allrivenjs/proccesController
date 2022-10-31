173096d12654c32b78f45b838bf5e58db80abf97
Allrivenjs <jaimeruizxdd2@gmail.com>
2022-10-30 20:28:24 -0500

    creo que funciona

--- algorithmMethod/Processes.ts
+++ algorithmMethod/Processes.ts
@@ -17,30 +17,41 @@ export class Processes {
         //la descripcion del procesos, es la descripcion del grupo + los datos del proceso.
 
         let i = 0;
-        while ( i <= groupProcess.processes.length) {
-            for (let j = 0; j < quantum; j++) {
-                // verificar si el proceso esta pausado
-                this.pauseProcess(groupProcess);
-                const process = groupProcess.processes[i];
-                //process is running
-                process.status = 'running';
-                //save the process in a file
-                const realburst = (process.burstTime / groupProcess.TH);
-                if (realburst <= process.getLengthProcess(groupProcess.description)) {
-                    const position = process.getLengthProcess(groupProcess.description) - realburst;
-                    WriteForFile.writeForFile(`${path}/${process.COMMAND}.txt`, process.getCharForDescriptionPosition(position));
+        let stop = false;
+        let processFinished = [];
+
+       while(!stop){
+            while (i <= groupProcess.processes.length) {
+                for (let j = 0; j < quantum; j++) {
+                    // verificar si el proceso esta pausado
+                    this.pauseProcess(groupProcess);
+                    const process = groupProcess.processes[i];
+                    //process is running
+                    process.status = 'running';
+                    //save the process in a file
+                    const realBurst = (process.burstTime / groupProcess.TH);
+                    if (realBurst <= process.getLengthProcess(groupProcess.description)) {
+                        const position = process.getLengthProcess(groupProcess.description) - realBurst;
+                        WriteForFile.writeForFile(`${path}/${process.COMMAND}.txt`, process.getCharForDescriptionPosition(position));
+                    }
+                    //decrease the burst time
+                    process.burstTime--;
+                    //if the burst time is 0, the process is finished
+                    if (process.burstTime === 0) {
+                        process.status = 'finished';
+                        //agregamos el proceso a la lista de procesos terminados
+                        processFinished.push(process);
+                        //si el proceso termino, lo eliminamos de la lista de procesos
+                        groupProcess.processes.splice(i, 1);
+                        i--;
+                        break;
+                    }
                 }
-                //decrease the burst time
-                process.burstTime--;
-                //if the burst time is 0, the process is finished
-                if (process.burstTime === 0) {
-                    process.status = 'finished';
-                    groupProcess.processes.splice(i, 1);
-                    i--;
-                    break;
-                }
+                i++;
             }
-            i++;
+        }
+        if (groupProcess.processes.length === 0) {
+            stop = true;
         }
     }
 
