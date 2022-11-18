83938e99ddbe84ad43e1023b8e16ee635603547a
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2022-11-17 21:05:08 -0500

    Update procces.ts

--- models/procces.ts
+++ models/procces.ts
@@ -97,7 +97,7 @@ export class Process {
 
     public setupProcess(catalogDescription: string, TH: number) {
         this.absoluteDescription = 'Catalog info: \n' + catalogDescription;
-        this.burstTime = parseInt(TH * this.absoluteDescription.length + '');
+        this.burstTime = parseInt((TH * this.absoluteDescription.length).toString());
 
         this.COMMAND = this.COMMAND[0].replace('/', '');
         this.status = 'ready';
